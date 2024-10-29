26e2f34d189bd48176793f3d31c83978abc3706a
Maxim Verzhbitskii <143388339+MaximVerzh@users.noreply.github.com>
2024-10-29 18:02:11 +0300

    num3.py

--- num3.py
+++ num3.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Чтение данных из файла с явным указанием разделителя
 data = pd.read_csv(r'C:\Users\Вержбицкая Полина\Downloads\students.csv', sep=';', header=None, names=['Преподаватель', 'Группа', 'Оценка'])
 
-# График распределения оценок по преподавателям
 data.groupby(['Преподаватель', 'Оценка']).size().unstack().plot(kind='bar', stacked=True)
 plt.title('Распределение оценок по преподавателям')
 plt.xlabel('Преподаватель')
 plt.ylabel('Количество')
 plt.show()
-
-# График распределения оценок по группам
 data.groupby(['Группа', 'Оценка']).size().unstack().plot(kind='bar', stacked=True)
 plt.title('Распределение оценок по группам')
 plt.xlabel('Группа')
